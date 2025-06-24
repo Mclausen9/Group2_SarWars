89be1bbe37dbecd8435ec072f04b9705b60a04a5
Niteesh Saravanan <nsarava3@travelers.com>
2025-06-24 11:09:08 -0400

    fix for species

--- species.js
+++ species.js
@@ -1,46 +1,56 @@
-// Get species ID from query string
-const sp = new URLSearchParams(window.location.search);
-const id = sp.get('id');
-
-console.log(id)
-
-fetch(`http://localhost:9001/api/species/${id}`)
-
-    .then(response => {
-        if (!response.ok) {
-            throw new Error("Failed to fetch species data");
-        }
-        return response.json();
-    })
-    .then(data => {
-
-        if (data) {
-            document.getElementById("name").textContent = data.name;
-            document.getElementById("classification").textContent = data.classification;
-            document.getElementById("designation").textContent = data.designation;
-            document.getElementById("averagelifespan").textContent = data.average_lifespan;
-            // Add similar lines for other details
-            return data
-        } else {
-            document.getElementById("name").textContent = "Species not found";
-        }
-    })
-    .then(data => {
-        const baseUrl = `http://localhost:9001/api`;
-        async function fetchCharacters(species) {
-            const url = `${baseUrl}/species/${species?.id}/characters`;
-            const characters = await fetch(url)
-                .then(res => res.json())
-            return characters;
-        }
-        fetchCharacters(data)
-        let charactersUl = document.querySelector("#characters>ul")
-        const characterList = data?.characters?.map(character => `<li><a href="/character.html?id=${character.id}">${character.name}</li>`)
-        charactersUl.innerHTML = characterList.join("");
+let nameH1;
+let classificationSpan;
+let lifespanSpan;
+let designationSpan;
+let charactersUl;
+const baseUrl = `http://localhost:9001/api`;
 
 
-    })
-    .catch(error => {
-        console.error("Error:", error);
-        document.getElementById("name").textContent = "Error loading species data";
-    });
+addEventListener('DOMContentLoaded', () => {
+    nameH1 = document.querySelector('h1#name')
+    classificationSpan = document.querySelector('span#classification')
+    lifespanSpan = document.querySelector('span#averagelifespan')
+    designationSpan = document.querySelector('span#designation')
+    charactersUl = document.querySelector('#characters>ul');
+    const sp = new URLSearchParams(window.location.search)
+    const id = sp.get('id')
+    getSpecies(id)
+});
+
+async function getSpecies(id) {
+    let species;
+    try {
+        species = await fetchSpecies(id)
+        species.characters = await fetchCharacters(species)
+    }
+    catch (e) {
+        console.error(`Error reading species ${id} data.`, e.message)
+    }
+    renderSpecies(species)
+}
+
+async function fetchSpecies(id) {
+    let speciesUrl = `${baseUrl}/species/${id}`;
+    return await fetch(speciesUrl)
+        .then(res =>
+            res.json()
+        );
+}
+
+async function fetchCharacters(species) {
+    const url = `${baseUrl}/species/${species?.id}/characters`;
+    const characters = await fetch(url)
+        .then(res => res.json())
+    return characters;
+}
+
+const renderSpecies = species => {
+    console.log(species)
+    document.title = `Swapi - ${species?.name}`;
+    nameH1.textContent = species?.name;
+    classificationSpan.textContent = species.classification
+    lifespanSpan.textContent = species?.average_lifespan;
+    designationSpan.textContent = species?.designation;
+    const characterList = species?.characters?.map(character => `<li><a href="/character.html?id=${character.id}">${character.name}</li>`)
+    charactersUl.innerHTML = characterList.join("");
+}
